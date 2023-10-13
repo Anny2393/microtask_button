f277ee589225a18d4e21912a2db2e4ee6e1adecb
Anna Narushevich <annyit23@gmail.com>
2023-10-13 18:25:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from "react";
 import './App.css';
+import {Button} from "./Components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const Button1Foo = (subscriber:string, age: number) => {
+console.log(subscriber,age)
+    }
+
+    const Button2Foo = (subscriber:string, age: number) => {
+console.log(subscriber,age)
+    }
+
+    const Button3Foo = () => {
+        console.log("I'm a dumb button")
+    }
+
+    return (
+        <div className="App">
+            <Button name={"MyYouTubeChanel-1"}
+                    callBack={()=>Button1Foo("Roma",29)}/>
+            <Button name={"MyYouTubeChanel-2"}
+                    callBack={()=>Button2Foo("Ann", 30)}/>
+            <Button name={"Dumb button"} callBack={Button3Foo}/>
+        </div>
+    );
 }
 
 export default App;
